a14ae82916617690c49d1c3647b339106cd3601e
dmifotech <mail-dmifotech>
2025-02-18 20:24:55 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -5,13 +5,9 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 3000;
 
+
 server.use(express.json());
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+server.use(cors({ origin: "https://itservice.dmifotech.com" }));
 server.use(express.static('public'));
 
 
