255052af321daa5a1eafaff051b9495718b90252
CHIRAG TYAGI <69377702+chiragtyagi86@users.noreply.github.com>
2025-02-18 09:35:09 +0530

    check_auth

--- client/assets/js/forms.js
+++ client/assets/js/forms.js
@@ -1,20 +1,37 @@
-//login-form
-document.getElementById("login-form").addEventListener("submit", function(e){
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const formObj = {};
-    formData.forEach((value, key) => formObj[key] = value);
-    console.log(formObj);
-    // Add your AJAX request here
-    // Example:
-    // fetch('/login', {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify(formObj)
-    // })
-    //    .then(response => response.json())
-    //    .then(data => console.log(data));
-    
-});
+async function checkAuth() {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      window.location.href = "/login.html";
+      return;
+    }
+
+    try {
+      const response = await fetch("xyphor-nexus87.dmifotech.com/dashboard", {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.status === 401 || response.status === 403) {
+        window.location.href = "/login.html";
+      } else if (!response.ok) {
+        console.error("Error:", await response.text());
+        window.location.href = "/login.html";
+      } else {
+        const data = await response.json();
+
+
+        // document.getElementById("user").innerHTML = data.message;
+        console.log(data.message);
+        
+
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      window.location.href = "/login.html";
+    }
+  }
+
+  checkAuth();
